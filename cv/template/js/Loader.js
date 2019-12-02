ba162260f747d5e60c5b1a87a0a2206a741cc33e
SyrianSam <sami.k92@gmail.com>
2019-12-02 12:26:37 +0100

    Loader.js debugging tools

--- cv/template/js/Loader.js
+++ cv/template/js/Loader.js
@@ -1,4 +1,8 @@
 function Load(container, url, target){
 
-	$(container).load(url+' '+ target);
+	$(container).load(url+' '+ target, function(){
+
+		//callback function if i want to call some scripts after the load
+		console.log('load successful');
+	});
 }
